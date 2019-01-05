2233fc1cd7c357adcf4a28f71f7dd1f987d9cf7b
Rishabh Mishra <mishrarishabh865@gmail.com>
2019-01-05 20:52:14 +0530

    add card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
-const Card = () => {
-  return <div>Card</div>;
+const CardBase = styled.div`
+  border: 1px solid rgba(14, 46, 71, 0.11);
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 8px;
+  box-shadow: 0px 4px 32px 4px rgba(45, 12, 120, 0.07);
+  padding: 16px;
+  display: inline-flex;
+  position: relative;
+  width: max-content;
+  margin: 8px;
+`;
+
+const Card = props => {
+  return <CardBase>{props.children}</CardBase>;
 };
 
 export default Card;
--- src/components/Card/Card.story.js
+++ src/components/Card/Card.story.js
@@ -2,4 +2,16 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Card from "./Card";
 
-storiesOf("Card", module).add("with text", () => <Card />);
+storiesOf("Card", module).add("with text", () => (
+  <div>
+    <Card>
+      Basic Card <br /> Multi Line
+    </Card>
+    <Card>
+      Basic Card <br /> Multi Line
+    </Card>
+    <Card>
+      Basic Card <br /> Multi Line
+    </Card>
+  </div>
+));
